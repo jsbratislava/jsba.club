618075fb6117cbe8519253f57c5410cd4e99a7cd
Matei Copot <matei@copot.eu>
2018-01-23 15:57:52 +0100

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img src="./../assets/logo.png" />
+    <h1>Up soon!</h1>
+  </div>
+</template>
+<script>
+  export default {
+    data: {}
+  }
+</script>
+<style>
+  body {
+    background-color: #333; 
+  }
+  img {
+    position: absolute;
+    top: calc(50% - 280px);
+    left: calc(50% - 256px);
+    box-shadow: 0 0 2px #111;
+  }
+  h1 {
+    position: absolute;
+    top: calc(50% + 256px);
+    left: calc(50% - 256px);
+    width: 512px;
+    text-align: center;
+    color: #888;
+    font-size: 80px;
+    margin-top: 0;
+  }
+</style>
